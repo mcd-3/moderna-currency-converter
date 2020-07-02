9822d0987f84460bb18070225a299f28dd8be63e
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2020-07-02 01:38:03 -0400

    Added fade in animation when the app starts

--- app/src/main/java/com/matthew/carvalhodagenais/modernaconverter/ui/ConvertFragment.kt
+++ app/src/main/java/com/matthew/carvalhodagenais/modernaconverter/ui/ConvertFragment.kt
@@ -6,25 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.matthew.carvalhodagenais.modernaconverter.R
-import kotlinx.android.synthetic.main.fragment_converter.*
 
 class ConvertFragment: Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_converter, container, false)
-        return view
+        return inflater.inflate(R.layout.fragment_converter, container, false)
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-    }
-
 }
