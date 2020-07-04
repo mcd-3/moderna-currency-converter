644e0cd1a3e5cbcb1382f673793e891745a5b3b8
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2020-07-04 01:52:20 -0400

    Prototyped conversion calculations

--- app/src/main/java/com/matthew/carvalhodagenais/modernaconverter/ui/ConvertFragment.kt
+++ app/src/main/java/com/matthew/carvalhodagenais/modernaconverter/ui/ConvertFragment.kt
@@ -1,12 +1,17 @@
 package com.matthew.carvalhodagenais.modernaconverter.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.content.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
 import com.matthew.carvalhodagenais.modernaconverter.MainActivity
 import com.matthew.carvalhodagenais.modernaconverter.R
 import kotlinx.android.synthetic.main.fragment_converter.*
@@ -29,14 +34,27 @@ class ConvertFragment: Fragment() {
     }
 
     private val submitButtonOnClick = View.OnClickListener {
-        submit_button.isClickable = false
-        (activity as MainActivity).convertViewModel.getRates(currency_button_left.text.toString()).observe(this, Observer {
-            if (it != null) {
-                Log.e("THINGY", it.rates[currency_button_right.text.toString()].toString())
+        if (currency_edit_text.text.toString().trim() != "") {
+
+            val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view?.windowToken, 0)
+
+            submit_button.isClickable = false
+
+            (activity as MainActivity).convertViewModel.getRates(currency_button_left.text.toString()).observe(this, Observer {
+                if (it != null) {
+                    base_text.text = "${it.base} ${currency_edit_text.text}"
+                    conversion_text.text =
+                        "${currency_button_right.text} ${(currency_edit_text.text.toString().toDouble()) * (it.rates[currency_button_right.text].toString().toDouble())}"
+                    content_motion_layout.setTransition(R.id.start_to_end)
+                    content_motion_layout.transitionToEnd()
+                } else {
+                    // 2 things: 1) no internet 2) no response
+                }
                 submit_button.isClickable = true
-                content_motion_layout.setTransition(R.id.start_to_end)
-                content_motion_layout.transitionToEnd()
-            }
-        })
+            })
+        } else {
+            Snackbar.make(content_motion_layout, "Enter an amount", Snackbar.LENGTH_LONG).show()
+        }
     }
 }
