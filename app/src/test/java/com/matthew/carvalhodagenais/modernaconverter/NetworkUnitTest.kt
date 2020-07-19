734c54491543d19319575a4e05942470db79f48d
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2020-07-18 20:15:15 -0400

    Added NetworkUnitTest.kt to test for network connectivity

---
+++ app/src/test/java/com/matthew/carvalhodagenais/modernaconverter/NetworkUnitTest.kt
@@ -0,0 +1,24 @@
+package com.matthew.carvalhodagenais.modernaconverter
+
+import androidx.lifecycle.LiveData
+import com.matthew.carvalhodagenais.modernaconverter.data.entities.ExchangeRate
+import com.matthew.carvalhodagenais.modernaconverter.viewmodels.ConvertViewModel
+import org.junit.Assert.*
+import org.junit.Test
+
+/**
+ * Unit tests to see if network connectivity is working as intended
+ */
+class NetworkUnitTest {
+
+    /**
+     * Check to see if the Exchange Rate server is returning data
+     */
+    @Test
+    fun isNetworkAPIWorking() {
+        val viewModel: ConvertViewModel = ConvertViewModel()
+        val baseCurrency: String = "EUR"
+        val rates: LiveData<ExchangeRate> = viewModel.getRates(baseCurrency)
+        assert(rates.value == null)
+    }
+}
